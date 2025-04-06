f0ca9c044cc5833f86dc7d4bbadf99031e5bdbe1
Rozthegray <rozthegrey@gmail.com>
2025-04-06 07:51:31 -0700

    Update models.py

--- tracker/models.py
+++ tracker/models.py
@@ -9,6 +9,29 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
     REQUIRED_FIELDS = ['email']
 
+from django.db import models
+from django.contrib.auth.models import User
+
+class Activity(models.Model):
+    ACTIVITY_TYPES = [
+        ('Running', 'Running'),
+        ('Cycling', 'Cycling'),
+        ('Weightlifting', 'Weightlifting'),
+        ('Swimming', 'Swimming'),
+        ('Walking', 'Walking'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="activities")
+    activity_type = models.CharField(max_length=50, choices=ACTIVITY_TYPES)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    distance = models.FloatField(blank=True, null=True, help_text="Distance in km")
+    calories_burned = models.PositiveIntegerField()
+    date = models.DateField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.activity_type} on {self.date}"
+
+
 class Activity(models.Model):
     ACTIVITY_TYPES = [
         ('running', 'Running'),
