32dc71cc738bf6a6e98032bc5d4b785358cfe64c
Rozthegray <rozthegrey@gmail.com>
2025-04-06 11:53:48 -0700

    Update models.py

--- tracker/models.py
+++ tracker/models.py
@@ -1,15 +1,4 @@
 from django.db import models
-
-# Create your models here.
-
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    REQUIRED_FIELDS = ['email']
-
-from django.db import models
 from django.contrib.auth.models import User
 
 class Activity(models.Model):
@@ -17,34 +6,14 @@ class Activity(models.Model):
         ('Running', 'Running'),
         ('Cycling', 'Cycling'),
         ('Weightlifting', 'Weightlifting'),
-        ('Swimming', 'Swimming'),
-        ('Walking', 'Walking'),
     ]
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="activities")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
     activity_type = models.CharField(max_length=50, choices=ACTIVITY_TYPES)
-    duration = models.PositiveIntegerField(help_text="Duration in minutes")
-    distance = models.FloatField(blank=True, null=True, help_text="Distance in km")
-    calories_burned = models.PositiveIntegerField()
-    date = models.DateField()
-
-    def __str__(self):
-        return f"{self.user.username} - {self.activity_type} on {self.date}"
-
-
-class Activity(models.Model):
-    ACTIVITY_TYPES = [
-        ('running', 'Running'),
-        ('cycling', 'Cycling'),
-        ('weightlifting', 'Weightlifting'),
-        ('swimming', 'Swimming'),
-    ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    activity_type = models.CharField(choices=ACTIVITY_TYPES, max_length=50)
     duration = models.PositiveIntegerField()
-    distance = models.FloatField(blank=True, null=True)
+    distance = models.FloatField(null=True, blank=True)
     calories_burned = models.PositiveIntegerField()
     date = models.DateField()
 
     def __str__(self):
-        return f"{self.activity_type} by {self.user.username}"
+        return f"{self.activity_type} - {self.user.username} - {self.date}"
